83b7852d079bf02d550e6f0b5d077329f2e78373
Diego Marques <marques.diego85@gmail.com>
2021-01-07 11:13:05 -0300

    atualizando para android X

---
+++ android/app/src/main/kotlin/com/dam/trivia/flutter_triviagame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dam.trivia.flutter_triviagame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\asus\Downloads\trivia_example-master\trivia_example-master"
+export "FLUTTER_ROOT=D:\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\diego.marques\IdeaProjects\flutter_triviagame"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=D:\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
